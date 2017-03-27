117fcdc3d88ce83b2d5c869318860a23ff9c3615
RubenDewitte <ruben.dewitte@student.howest.be>
2017-03-27 14:01:47 +0200

    Testing

---
+++ Sudoku/src/sudoku/Program.java
@@ -0,0 +1,9 @@
+package sudoku;
+
+public class Program {
+    public static void main(String[] args) {
+        Sudoku s = new Sudoku();
+        s.consolePrintSudoku();
+        
+    }
+}
--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -1,9 +1,23 @@
 package sudoku;
 
+import java.util.Arrays;
+
 public class Sudoku {
-
-    public static void main(String[] args) {
-
-    }
     
+    private static final int BOARD_WIDTH = 9;
+    private static final int BOARD_HEIGHT = 9;
+    private int[][] grid;
+
+    public Sudoku() {
+        grid = new int[BOARD_HEIGHT][BOARD_WIDTH];
+    }
+
+    public void consolePrintSudoku() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
